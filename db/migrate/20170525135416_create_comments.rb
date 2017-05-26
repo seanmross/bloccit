aed33027c5b2e94665b8b9177fabe7c05e7b92cd
Sean Ross <seanmross9@gmail.com>
2017-05-25 22:21:33 -0400

    updated create comments db file, does not need index for rails version 5.1

--- db/migrate/20170525135416_create_comments.rb
+++ db/migrate/20170525135416_create_comments.rb
@@ -2,9 +2,9 @@ class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
       t.text :body
-      t.references :post, index: true, foreign_key: true
+      t.references :post, foreign_key: true
 
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
