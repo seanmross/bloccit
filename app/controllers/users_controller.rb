cf1699aaaf85deaeee1004f4ca2bfa5b75f80755
Sean Ross <seanmross9@gmail.com>
2017-06-08 22:27:44 -0400

    Assignment-24: complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,28 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
   end
 
   def create
-# #9
-  @user = User.new
-  @user.name = params[:user][:name]
-  @user.email = params[:user][:email]
-  @user.password = params[:user][:password]
-  @user.password_confirmation = params[:user][:password_confirmation]
+    @user = User.new(params_for_user)
 
-# #10
-  if @user.save
-    flash[:notice] = "Welcome to Bloccit #{@user.name}!"
-    redirect_to root_path
-  else
-    flash.now[:alert] = "There was an error creating your account. Please try again."
-    render :new
+    if @user.save
+      flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "There was an error creating your account. Please try again."
+      render :new
+    end
   end
-end
+
+  def confirm
+    @user = User.new(params_for_user)
+  end
+
+  private
+
+  def params_for_user
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
