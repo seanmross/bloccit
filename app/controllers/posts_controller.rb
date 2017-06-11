d32f6f55ea2c2946816c1acf052d36e26fb00699
Sean Ross <seanmross9@gmail.com>
2017-06-11 01:11:10 -0400

    Assignment-28: complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :require_sign_in, except: :show
   before_action :authorize_user, except: [:show, :new, :create]
+
   def show
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
@@ -60,7 +61,7 @@ class PostsController < ApplicationController
 
   def authorize_user
     post = Post.find(params[:id])
-    unless current_user == post.user || current_user.admin?
+    unless current_user == post.user || current_user.admin? || current_user.moderator?
       flash[:alert] = "You must be an admin to do that."
       redirect_to [post.topic, post]
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,5 @@ class User < ApplicationRecord
 
   # #6
   has_secure_password
-  enum role: [:member, :admin]
+  enum role: [:member, :admin, :moderator]
 end
