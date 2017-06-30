433cc5b4262ddbfc33502dd3dde981efc8b021df
Sean Ross <seanmross9@gmail.com>
2017-06-29 21:35:26 -0400

    production env updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,6 @@ class PostsController < ApplicationController
   def create
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.build(post_params)
-    @post.user = current_user
 
     if @post.save
       flash[:notice] = "Post was saved."
